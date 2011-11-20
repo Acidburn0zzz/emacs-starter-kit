8efbc3f573982e4ef14757bb1bcf6f9b9a58e095
Taek Joo Kim <taicki@gmail.com>
2011-11-19 19:06:14 -0800

    add my setting

---
+++ taicki.el
@@ -0,0 +1,6 @@
+(setq indent-tabs-mode nil
+      make-backup-file nil
+      whitespace-style '(trailing empty tabs)
+      )
+(setq show-trailing-whitespace t)
+(setq require-final-newline 'query)
